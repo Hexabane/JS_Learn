5cc3cc04acaf2a11033f5d65e23383cfefbba253
Kunal Goyal <hexabaneofficial@gmail.com>
2022-03-16 20:18:51 +0200

    API

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
